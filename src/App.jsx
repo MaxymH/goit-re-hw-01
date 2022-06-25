8ca7a81e8d1f0cbc4e944fab3954c8a3f9cdbccf
Max_Hrytsenia <95626499+MaxymH@users.noreply.github.com>
2022-06-25 17:12:26 +0200

    repair

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,8 @@ import dataUsers from './components/data/user.json';
 import dataStatics from './components/data/statics.json';
 import dataFriends from './components/data/friends.json';
 import dataTransactions from './components/data/transactions.json'
-
-import { computeHeadingLevel } from "@testing-library/react";
-
 export const App = () => {
   const { username, tag, location, avatar, stats } = dataUsers
-  const { label, percentage,id } = dataStatics[0]
   return (
     <div>
       <User
